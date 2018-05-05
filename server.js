b94a836fe226c83448c655b0ff52578871b859b5
Ivan Wei <scorpionseed@gmail.com>
2018-05-06 00:49:15 +0800

    fix: sendfile path

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ app.use('/public', express.static(__dirname + '/public'));
 
 //app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/yuting', function (req, res) {
-    res.sendFile('yuting.html');
+    res.sendFile(__dirname + '/yuting.html');
 })
 
 app.get('/judy', function (req, res) {
-    res.sendFile('judy.html');
+    res.sendFile(__dirname + '/judy.html');
 })
 
 app.get('/mike', function (req, res) {
-    res.sendFile('mike.html');
+    res.sendFile(__dirname + '/mike.html');
 })
 
 app.listen(3000, function () {

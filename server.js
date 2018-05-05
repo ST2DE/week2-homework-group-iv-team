46efaf521848c4103b7b0208b74548024dbc4169
Yu-Ting, You <lovely31221h@gmail.com>
2018-05-06 00:22:20 +0800

    delete { root: __dirname }

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ app.use('/public', express.static(__dirname + '/public'));
 
 //app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html');
 })
 
 app.get('/yuting', function (req, res) {
-    res.sendFile('yuting.html', { root: __dirname });
+    res.sendFile('yuting.html');
 })
 
 app.get('/judy', function (req, res) {
-    res.sendFile('judy.html', { root: __dirname });
+    res.sendFile('judy.html');
 })
 
 app.get('/mike', function (req, res) {
-    res.sendFile('mike.html', { root: __dirname });
+    res.sendFile('mike.html');
 })
 
 app.listen(3000, function () {
